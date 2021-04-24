842ea8f820d7965b1d9c7d3f135dedfa3ed87b48
rupi482021 <77381517+rupi482021@users.noreply.github.com>
2021-04-24 10:46:43 +0300

    modeles

---
+++ project_new/project_new/Models/Profile.cs
@@ -0,0 +1,33 @@
+﻿using Kaatsu.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using Content_Upload_Model.Models.DAL;
+
+namespace Content_Upload_Model.Models
+{
+    public class Profile
+    {
+        
+        string mail;
+        string password;
+
+        public Profile() { }
+
+        public Profile(string mail, string password)
+        {
+            Mail = mail;
+            Password = password;
+        }
+
+        public string Mail { get => mail; set => mail = value; }
+        public string Password { get => password; set => password = value; }
+
+        public string Insert()
+        {
+            DBServices dbs = new DBServices();
+            return dbs.InsertProfile(this);
+        }
+    }
+}
---
+++ project_new/project_new/Models/video.cs
@@ -0,0 +1,38 @@
+﻿using Kaatsu.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kaatsu.Models
+{
+    public class video
+    {
+
+       int videoId ;
+       string description;
+       string caption;
+       string subtitlepath;
+
+        public video(int videoId, string description, string caption, string subtitlepath)
+        {
+            VideoId = videoId;
+            Description = description;
+            Caption = caption;
+            Subtitlepath = subtitlepath;
+        }
+
+        public video() { }
+
+        public int VideoId { get => videoId; set => videoId = value; }
+        public string Description { get => description; set => description = value; }
+        public string Caption { get => caption; set => caption = value; }
+        public string Subtitlepath { get => subtitlepath; set => subtitlepath = value; }
+
+        public List<video> getvideoList (int id)
+        {
+            DBServices dbs = new DBServices();
+            return dbs.getvideoList(id);
+        }
+    }
+}
