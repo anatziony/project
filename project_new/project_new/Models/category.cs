f0e97e9f75964a5f5224d604c9aa28e70d88e9a9
rupi482021 <77381517+rupi482021@users.noreply.github.com>
2021-04-24 11:02:36 +0300

    ajax

--- project_new/project_new/Models/category.cs
+++ project_new/project_new/Models/category.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Content_Upload_Model.Models.DAL;
+//using Content_Upload_Model.Models.DAL;
 using Kaatsu.Models.DAL;
 
 namespace Content_Upload_Model.Models
